aa714d7da4052eef11547d8bb2f4fbd32aa72d7e
mat-kossowski <mat.kossowski.pl@gmail.com>
2022-09-21 10:01:39 +0200

    zsdfgs

--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -1,6 +1,9 @@
-// const AddToCart = document.querySelector('#addProduct')
-//
-//
-// const product = AddToCart.dataset.id
-// console.log(product)
+const AddToCart = document.getElementsByClassName('addProduct')
 
+for(let cart of AddToCart){
+    cart.addEventListener("click", addItemToCart)
+}
+
+async function addItemToCart(event){
+    return await fetch('/add?prod_id=' +event.target.dataset.id)
+}
