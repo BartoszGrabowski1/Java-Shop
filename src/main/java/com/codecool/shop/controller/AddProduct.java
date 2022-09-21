1c32d309bb8aa9e470f97f974866bde070d652c3
mat-kossowski <mat.kossowski.pl@gmail.com>
2022-09-21 09:30:38 +0200

    add to cart

--- src/main/java/com/codecool/shop/controller/AddProduct.java
+++ src/main/java/com/codecool/shop/controller/AddProduct.java
@@ -1,5 +1,10 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +18,12 @@ public class AddProduct extends HttpServlet {
 
      @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         CartDao cartDaoStore = CartDaoMem.getInstance();
+        ProductDao productDataStore = ProductDaoMem.getInstance();
            String id =  req.getParameter("prod_id");
          System.out.println(id);
+         int productId = Integer.parseInt(id);
+        cartDaoStore.addProduct(productDataStore.find(productId));
+        resp.sendRedirect("/");
      }
 }
