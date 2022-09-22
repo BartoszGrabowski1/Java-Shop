51210dc3b79e466bdc4595188eca0d1deb1a63d3
Maciej Strękowski <maciejstrekowski@gmail.com>
2022-09-22 09:22:48 +0200

    payments merged

---
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -0,0 +1,33 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.service.CartService;
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "PaymentServlet", urlPatterns = {"/payment"})
+public class PaymentController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CartDao cartDaoStore = CartDaoMem.getInstance();
+        CartService cartService = new CartService(cartDaoStore);
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("cart", cartService.getSelectedProducts());
+        context.setVariable("value", cartService.getValue());
+
+        engine.process("product/indexpayment.html", context, resp.getWriter());
+    }
+}
