4f358b8f8b2312bbe436f18bdaf0cb06bb5f01f1
BartoszGrabowski1 <heksargonh@gmail.com>
2022-10-05 14:34:25 +0200

    end of shift commit

--- src/main/java/com/codecool/shop/controller/AddProductController.java
+++ src/main/java/com/codecool/shop/controller/AddProductController.java
@@ -2,6 +2,7 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.database.DataBaseManager;
 import com.codecool.shop.dao.implementation.CartDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 
@@ -11,6 +12,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 @WebServlet(name = "AddToCartServlet", urlPatterns = {"/add"})
@@ -25,5 +30,24 @@ public class AddProductController extends HttpServlet {
         int productId = Integer.parseInt(id);
         cartDaoStore.addProduct(productDataStore.find(productId));
         resp.sendRedirect("/");
+        saveProductFromCartToDB(productId, 1);
     }
+
+    void saveProductFromCartToDB(int productId, int userId){
+        System.out.println("!!! FUNKCJA WESZLA UWAGA !!!");
+        PreparedStatement preparedStatement = null;
+        try(Connection connection = DataBaseManager.dataSource.getConnection())  {
+            String sql = "INSERT INTO cart (product_id, user_id) VALUES (?,?)";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, productId);
+            preparedStatement.setInt(2, userId);
+            preparedStatement.executeQuery();
+//            ResultSet resultSet = connection.createStatement().executeQuery(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("!!! FUNKCJA PRZESZLA UWAGA !!!");
+    }
+
+
 }
--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,7 +1,10 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.database.DataBaseManager;
 import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.service.CartService;
 import com.codecool.shop.config.TemplateEngineUtil;
 import org.thymeleaf.TemplateEngine;
@@ -14,6 +17,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 @WebServlet(name = "CartServlet", urlPatterns = {"/cart"})
@@ -21,6 +28,7 @@ public class CartController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("WSZEDLEM DO KARTA EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE ");
         CartDao cartDaoStore = CartDaoMem.getInstance();
         CartService cartService = new CartService(cartDaoStore);
         HttpSession session = req.getSession();
@@ -32,5 +40,24 @@ public class CartController extends HttpServlet {
         context.setVariable("name", session.getAttribute("name"));
 
         engine.process("cart/cart.html", context, resp.getWriter());
+        loadCartFromDB(1);
     }
+
+    void loadCartFromDB(int userId){
+        CartDao cartDaoStore = CartDaoMem.getInstance();
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+
+        try(Connection connection = DataBaseManager.dataSource.getConnection())  {
+            String sql = "SELECT c.product_id, p.name, p.price FROM cart c, product p WHERE c.user_id =" + userId + " AND p.id = c.product_id";
+            ResultSet resultSet = connection.createStatement().executeQuery(sql);
+            while(resultSet.next()){
+                cartDaoStore.addProduct(productDataStore.find(resultSet.getInt(1)));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 }
